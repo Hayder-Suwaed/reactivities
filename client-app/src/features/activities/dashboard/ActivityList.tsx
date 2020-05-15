ae4c7920f8d2acd4e3a580baf228cb41e196152c
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-15 02:19:49 -0600

    Added the Activity Details components

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -13,11 +13,13 @@ const ActivityList: React.FC<IProps> = ({ activities }) => {
         {activities.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
-        <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Header as="a">{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
-                <div>{activity.city}, {activity.venue}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
               </Item.Description>
               <Item.Extra>
                 <Button floated="right" content="View" color="blue" />
