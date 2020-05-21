fa42043805928259d3e2b5df30fc19270f2d582b
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-21 10:18:22 -0600

    pull from repo

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
 
-interface IProps {
+interface IProp {
   setEditMode: (editMode: boolean) => void;
+  activity: IActivity | null;
 }
-
-const ActivityForm: React.FC<IProps> = ({ setEditMode }) => {
+ 
+export const ActivityForm: React.FC<IProp> = ({
+  setEditMode,
+  activity: initialFormData
+}) => {
+  const initializeForm = () => {
+    if (initialFormData) {
+      return initialFormData;
+    } else {
+      return {
+        id: "",
+        title: "",
+        description: "",
+        category: "",
+        date: "",
+        city: "",
+        venue: ""
+      };
+    }
+  };
+ 
+  const [activity, setActivity] = useState<IActivity>(initializeForm);
+ 
   return (
     <Segment clearing>
       <Form>
-        <Form.Input placeholder="Title" />
-        <Form.TextArea rows={2} placeholder="Description" />
-        <Form.Input placeholder="Category" />
-        <Form.Input type="date" placeholder="Date" />
-        <Form.Input placeholder="City" />
-        <Form.Input placeholder="Venue" />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Form.Input placeholder="Title" value={activity.title} />
+        <Form.TextArea
+          rows={2}
+          placeholder="Description"
+          value={activity.description}
+        />
+        <Form.Input placeholder="Category" value={activity.category} />
+        <Form.Input type="date" placeholder="Date" value={activity.date} />
+        <Form.Input placeholder="City" value={activity.city} />
+        <Form.Input placeholder="Venue" value={activity.venue} />
+        <Button floated="right" positive type="submit" content="Create" />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
