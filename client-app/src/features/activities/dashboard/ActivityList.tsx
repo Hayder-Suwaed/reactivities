90183291bdf64b4786372226da7ea45c4a4c148b
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-26 21:47:04 -0600

    CleanUp Code

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,9 +1,7 @@
-import React, { SyntheticEvent, useContext } from "react";
+import React, { useContext } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
 import { observer } from "mobx-react-lite";
 import ActivityStore from "../../../app/stores/activityStore";
-
 
 const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
