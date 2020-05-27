501401e295ab32de591b02c1adb5008abea618a1
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-27 09:17:38 -0600

    Enabling MobX Strict Moe

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,6 +3,7 @@ import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
 import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 interface IProp {
   activity: IActivity | null;
@@ -110,4 +111,4 @@ export const ActivityForm: React.FC<IProp> = ({
   );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
