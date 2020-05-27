1b156d4317930fe17f7e60cf8685b1ca0674d4d4
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-27 10:58:54 -0600

    Setiing up my Routes

---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const HomePage = () => {
+  return (
+    <Container style={{ margin: "7em" }}>
+      <h1>Home Page</h1>
+    </Container>
+  );
+};
+
+export default HomePage;
