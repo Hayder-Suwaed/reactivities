3b985e6545e23b1e55d24261e8b5cbdc1dff1af0
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-29 15:32:42 -0600

    Moved Home Page outside of navigation routes

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <Container style={{ margin: "7em" }}>
       <h1>Home Page</h1>
+      <h1>
+        Go to <Link to="/activities">Activities</Link>
+      </h1>
     </Container>
   );
 };
