a1fb9f80262a072ef52ff6c73ccaa01dce93e3a9
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-06-04 23:50:29 -0600

    CleanUp Code

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,7 +2,6 @@ import "mobx-react-lite/batchingForReactDom";
 import React, { useContext, Fragment } from "react";
 import { Item, Label } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
-import ActivityStore from "../../../app/stores/activityStore";
 import ActivityListItem from "./ActivityListItem";
 import { RootStoreContext } from "../../../app/stores/rootStore";
 
