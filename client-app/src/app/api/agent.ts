1f115e37df98ed0e1f8b25fd39920ea3f970a439
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-06-05 13:20:36 -0600

    Used Axios Request Interceptors to Automatically Send the JWT Token

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,13 +3,23 @@ import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
 import { IUser, IUserFormValues } from "../models/user";
-import { request } from "http";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
+axios.interceptors.request.use(
+  (config) => {
+    const token = window.localStorage.getItem("jwt");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 axios.interceptors.response.use(undefined, (error) => {
   if (error.message === "Network Error" && !error.response) {
-    toast.error("Network error - make sure API is running!!");
+    toast.error("Network error - make sure API is running!");
   }
   const { status, data, config } = error.response;
   if (status === 404) {
@@ -25,16 +35,15 @@ axios.interceptors.response.use(undefined, (error) => {
   if (status === 500) {
     toast.error("Server error - check the terminal for more info!");
   }
+
   throw error.response;
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
-
 const sleep = (ms: number) => (response: AxiosResponse) =>
   new Promise<AxiosResponse>((resolve) =>
     setTimeout(() => resolve(response), ms)
   );
-
 const requests = {
   get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
   post: (url: string, body: {}) =>
@@ -43,7 +52,6 @@ const requests = {
     axios.put(url, body).then(sleep(1000)).then(responseBody),
   del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
 };
-
 const Activities = {
   list: (): Promise<IActivity[]> => requests.get("/activities"),
   details: (id: string) => requests.get(`/activities/${id}`),
@@ -54,12 +62,14 @@ const Activities = {
 };
 
 const User = {
-  current: (): Promise<IUser> => requests.get('/user'),
-  login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
-  register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
-}
+  current: (): Promise<IUser> => requests.get("/user"),
+  login: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    requests.post(`/user/register`, user),
+};
 
 export default {
   Activities,
-  User
+  User,
 };
