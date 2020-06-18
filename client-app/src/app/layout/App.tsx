325b094409696d1180507c04f6ae9fa41a7dcab3
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-06-18 14:51:33 -0700

    Added Links and Profile Page Component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -17,6 +17,7 @@ import LoginForm from "../../features/user/LoginForm";
 import { RootStoreContext } from "../stores/rootStore";
 import LoadingComponent from "./LoadingComponent";
 import ModalContainer from "../common/modals/ModalContainer";
+import ProfilePage from "../../features/profiles/ProfilePage";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   const rootStore = useContext(RootStoreContext);
@@ -31,7 +32,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
     }
   }, [getUser, setAppLoaded, token]);
 
-  if (!appLoaded) return <LoadingComponent content='Loading app.....'/>
+  if (!appLoaded) return <LoadingComponent content="Loading app....." />;
 
   return (
     <Fragment>
@@ -52,7 +53,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                   path={["/createActivity", "/manage/:id"]}
                   component={ActivityForm}
                 />
-                <Route path="/login" component={LoginForm} />
+                <Route path="/profile/:username" component={ProfilePage} />
               </Switch>
             </Container>
           </Fragment>
