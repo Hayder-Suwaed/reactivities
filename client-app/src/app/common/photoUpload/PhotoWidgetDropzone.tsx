c2711775c7f93fe7fb7880b6be7a49d3da6c41bb
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-06-21 21:39:18 -0700

    added mssing dependences

--- client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -28,7 +28,7 @@ const PhotoWidgetDropZone: React.FC<IProps> = ({ setFiles }) => {
         })
       )
     );
-  }, []);
+  }, [setFiles]);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
