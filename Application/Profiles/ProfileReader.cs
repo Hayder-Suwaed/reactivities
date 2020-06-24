073db853861a1c4db90fbfcebbbd2643b71a8080
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-06-23 23:57:57 -0700

    Added a Profile reader helper class and interface

--- Application/Profiles/ProfileReader.cs
+++ Application/Profiles/ProfileReader.cs
@@ -20,26 +20,26 @@ namespace Application.Profiles
 
         public async Task<Profile> ReadProfile(string username)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x=> x.UserName == username);
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
 
             if (user == null)
-            throw new RestException(HttpStatusCode.NotFound, new {User = "Not found"});
+                throw new RestException(HttpStatusCode.NotFound, new { User = "Not found" });
 
-            var currentUser = await _context.Users.SingleOrDefaultAsync(x=> x.UserName == _userAccessor.GetCurrentUsername());
+            var currentUser = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUsername());
 
             var profile = new Profile
             {
                 DisplayName = user.DisplayName,
-                    Username = user.UserName,
-                    Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                    Photos = user.Photos,
-                    Bio = user.Bio,
-                    FollowersCount = user.Followers.Count(),
-                    FollowingCount = user.Followings.Count()
+                Username = user.UserName,
+                Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                Photos = user.Photos,
+                Bio = user.Bio,
+                FollowersCount = user.Followers.Count(),
+                FollowingCount = user.Followings.Count()
             };
 
-            if (currentUser.Followings.Any(x=> x.TargetId == user.Id))
-            profile.IsFollowed = true;
+            if (currentUser.Followings.Any(x => x.TargetId == user.Id))
+                profile.IsFollowed = true;
 
             return profile;
         }
