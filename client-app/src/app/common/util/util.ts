21e7abfae0a343d1e8a60bcc800a6abf5949ce65
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-07-04 22:43:18 -0700

    cleanup code

--- client-app/src/app/common/util/util.ts
+++ client-app/src/app/common/util/util.ts
@@ -2,12 +2,7 @@ import { IUser } from "../../models/user";
 import { IActivity, IAttendee } from "../../models/activity";
 
 export const combineDateAndTime = (date: Date, time: Date) => {
-  // const timeString = time.getHours() + ":" + time.getMinutes() + ":00";
-
-  // const year = date.getFullYear();
-  // const month = date.getMonth() + 1;
-  // const day = date.getDate();
-  // const dateString = `${year}-${month}-${day}`;
+  
   const dateString = date.toISOString().split("T")[0];
   const timeString = time.toISOString().split("T")[1];
 
