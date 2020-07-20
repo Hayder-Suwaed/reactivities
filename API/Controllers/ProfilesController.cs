77df815064caa99aab68438b5caebe2b69f07460
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-07-19 21:31:29 -0600

    cleanup code

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -13,16 +13,16 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Username = username });
         }
-         [HttpPut]
+        [HttpPut]
         public async Task<ActionResult<Unit>> Edit(Edit.Command command)
         {
             return await Mediator.Send(command);
         }
 
-      [HttpGet("{username}/activities")]
-        public async Task<ActionResult<List<UserActivityDto>>> GetUserActivities(string username, string predicate) 
+        [HttpGet("{username}/activities")]
+        public async Task<ActionResult<List<UserActivityDto>>> GetUserActivities(string username, string predicate)
         {
-            return await Mediator.Send(new ListActivities.Query{Username = username, Predicate = predicate});
+            return await Mediator.Send(new ListActivities.Query { Username = username, Predicate = predicate });
         }
     }
 }
